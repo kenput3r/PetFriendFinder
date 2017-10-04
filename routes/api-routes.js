7ff36f2231fceda9480d2fd2815ad3850644616b
kenput3r <devolve.wtf@gmail.com>
2017-10-04 08:16:12 -0700

    Moved OwnerImages to public folder, and changed paths in api/upload

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,10 @@
 const db = require('../models');
+const fileUpload = require('express-fileupload');
+const path = require("path");
 
 module.exports = function(app) {
+
+    app.use(fileUpload());
 
     //Get all pets
     app.get('/api/pets', function(req, res) {
@@ -47,5 +51,29 @@ module.exports = function(app) {
         }).then(function(dbOwners) {
             res.json(dbOwners);
         });
+    });
+
+    app.post("/api/upload", function(req, res){
+        
+        if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+        let sampleFile = req.files.sampleFile;
+    
+        var imgPath = "/OwnerImages/"+req.body.id+"_"+req.body.name+".jpg";
+        // Use the mv() method to place the file somewhere on your server
+        console.log(imgPath);
+        sampleFile.mv(path.join(__dirname,"../public"+imgPath), function(err) {
+            if (err)
+                return res.status(500).send(err);
+
+            db.Owners.update({
+            picture: imgPath
+            },{
+            where: { id: req.body.id}
+            });
+        
+            res.send('File uploaded!');
+        });
+   
     });
 }
