aac87e9aa0fe3130c194ab86ae67bfec94282ba4
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-10-06 13:43:09 -0700

    merge after pull with html-routes updated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,11 +8,19 @@ module.exports = function(app) {
             where: {id: req.params.id},
             include: [models.Pets]
         }).then(data => {
-            console.log(data.pets);
+            let canEdit = false;
+            if(req.isAuthenticated()) {
+                if((req.user.id * 1) === (req.params.id * 1)) {
+                    canEdit = true;
+                }
+            }
+            console.log(canEdit);
+            console.log(req.params.id, req.user.id);
             res.render('owner', {
                 name: data.name,
                 picture: data.picture,
                 pets: data.Pets,
+                canEdit: canEdit,
                 isUser: req.isAuthenticated()
             });
         });
@@ -39,19 +47,94 @@ module.exports = function(app) {
     //Get a pet by id
     app.get('/pets/:id', function(req, res) {
         models.Pets.findOne( {where: {id: req.params.id}}).then(data => {
+            let canEdit = false;
+            if(req.isAuthenticated()) {
+                if((data.OwnerId * 1) === (req.user.id * 1)) {
+                    canEdit = true;
+                }
+                console.log(data.OwnerId, req.user.id);
+            }
+            
             res.render('pet', {
                 name: data.name,
                 picture: data.picture,
+                canEdit: canEdit,
                 isUser: req.isAuthenticated()
-            })
-        })
-    })
+            });
+        });
+    });
 
     //Get all pets
     app.get('/pets', function(req, res) {
         models.Pets.findAll({}).then(data => {
             let Pets = [];
+            
+            for(pet in data) {
+                Pets.push(data[pet]);
+            }
 
+            res.render('pets', {
+                pets: Pets,
+                isUser: req.isAuthenticated()
+            });
+        });
+    });
+
+    app.post('/pets/filter', function(req, res) {
+        console.log("=========" + req.body.type + "========" + req.body.breed + "========" + req.body.age + "======" + req.body.gender);
+
+        //search age
+        var age = 0;
+        if(req.body.age === "0-3") {
+            age = {
+                lt: 3
+            }
+        } else if (req.body.age === "4-7") {
+            age = {
+                between: [4, 7]
+            }
+        } else {
+            age = {
+                gte: 8
+            }
+        };
+
+        //create form validation so that users HAVE to select a type
+
+        if(req.body.type != "") {
+            models.Pets.findAll({
+                where: {type: req.body.type,
+                        //breed: req.body.breed,
+                        //age: req.body.age,
+                        gender: req.body.gender}
+            }).then(data => {
+    
+                let Pets = [];
+    
+                
+                //push pet data to Pets
+                for(pet in data) {
+                    Pets.push(data[pet]);
+                }
+    
+                res.render('pets', {
+                    pets: Pets,
+                    isUser: req.isAuthenticated()
+                });
+            });
+        }
+
+        models.Pets.findAll({
+            where: {type: req.body.type,
+                    breed: req.body.breed,
+                    age: req.body.age,
+                    gender: req.body.gender}
+        }).then(data => {
+
+            let Pets = [];
+
+            
+            //push pet data to Pets
             for(pet in data) {
                 Pets.push(data[pet]);
             }
