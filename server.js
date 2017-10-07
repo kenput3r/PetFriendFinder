5f06bdd0386953123fca3086f2ad0e254589d59b
Sandro Wong <saloiofun@gmail.com>
2017-10-06 22:59:32 -0700

    change dashboard path and add modal to add pet

--- server.js
+++ server.js
@@ -11,11 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
 
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,7 +32,13 @@ app.use(express.static('public'));
 app.use(fileUpload());
 
 app.set('views', './views');
-app.engine('hbs', exphbs({ defaultLayout: "main", extname: '.hbs' }));
+app.engine('hbs', exphbs({
+    defaultLayout: "main",
+    extname: '.hbs',
+    partialsDir: [
+        __dirname + '/views/partials',
+    ]
+}));
 app.set('view engine', '.hbs');
 
 require('./config/passport/passport.js')(passport, models.Owners);
@@ -33,7 +47,9 @@ require('./routes/api-routes.js')(app, fileUpload);
 require('./routes/pet-api.js')(app, fileUpload);
 require('./routes/html-routes.js')(app);
 
-models.sequelize.sync({ force: false }).then(() => {
+models.sequelize.sync({
+    force: false
+}).then(() => {
     app.listen(PORT, () => {
         console.log('App listening on PORT ' + PORT);
     });
