48331a523a68f5a58589f2ed5e2f7f0f33ba2037
kenput3r <devolve.wtf@gmail.com>
2017-10-08 14:01:41 -0700

    add tab functionality to owner.hbs

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -6,3 +6,12 @@ $('#type').change(function() {
         $('#submit').attr('disabled', true).addClass('disabled');
     }
 });
+
+$('.section-change').click(function() {
+    let hideId = $('.section-change.active').attr('data-section');
+    let showId = $(this).attr('data-section');
+    $('.section-change.active').removeClass('active');
+    $(this).addClass('active');
+    $(hideId).addClass('d-none');
+    $(showId).removeClass('d-none');
+});
