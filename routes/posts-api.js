d28f9a00c9f1b8c94f884357dbc94a1b30649bc5
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-10-09 19:29:42 -0700

    fixing get all posts

--- routes/posts-api.js
+++ routes/posts-api.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     // Get all posts
     app.get('/api/posts', function(req, res){
         models.Posts.findAll().then(function(data){
-            res.json(dbPosts);
+            res.json(data);
         });
     });
 
@@ -13,11 +13,12 @@ module.exports = function(app){
         models.Posts.findAll({
             where: {OwnerId: req.params.ownerId},
             include: [models.Owners]
-        }).then(function(dbOwnerPosts){
-            res.json(dbOwnerPosts);
+        }).then(function(data){
+            res.json(data);
         });
     });
 
+    //create new post
     app.post('/api/posts', function(req, res){
         models.Posts.create({
             body: req.body.body,
@@ -27,10 +28,11 @@ module.exports = function(app){
         })
     });
 
+    //delete current user's post
     app.delete('/api/posts', function(req, res){
         models.Posts.destroy({
             where: {
-                OwnerId: req.body.id //or req.user.id
+                OwnerId: req.user.id
             }
         }).then(function(data){
             res.json(data);
