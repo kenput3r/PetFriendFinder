7bcdbb78c92a21c0180f1fffbd8924bfe3f480c6
kenput3r <devolve.wtf@gmail.com>
2017-10-11 14:14:03 -0700

    add redirects to rautes unless req.isAuthenticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,103 +4,114 @@ let loggedIn;
 module.exports = function (app) {
     //Get an owner by id
     app.get('/owners/:id', function (req, res) {
-        models.Owners.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [models.Pets]
-        }).then(data => {
-            models.Posts.findAll({
-                where: { 
-                    OwnerId: req.params.id
+        if(req.isAuthenticated()) {
+            models.Owners.findOne({
+                where: {
+                    id: req.params.id
                 },
-                order: [['createdAt', 'DESC']]
-            }).then(posts => {
-                let Posts = [];
-                for(post in posts) {
-                    Posts.push(posts[post].dataValues);
-                }
-                res.render('owner', {
-                    name: data.name,
-                    picture: data.picture,
-                    age: data.age,
-                    location: data.location,
-                    email: data.email,
-                    pets: data.Pets,
-                    bio: data.bio,
-                    posts: Posts,
-                    isUser: req.isAuthenticated()
+                include: [models.Pets]
+            }).then(data => {
+                models.Posts.findAll({
+                    where: { 
+                        OwnerId: req.params.id
+                    },
+                    order: [['createdAt', 'DESC']]
+                }).then(posts => {
+                    let Posts = [];
+                    for(post in posts) {
+                        Posts.push(posts[post].dataValues);
+                    }
+                    res.render('owner', {
+                        name: data.name,
+                        picture: data.picture,
+                        age: data.age,
+                        location: data.location,
+                        email: data.email,
+                        pets: data.Pets,
+                        bio: data.bio,
+                        posts: Posts,
+                        isUser: req.isAuthenticated()
+                    });
                 });
             });
-        });
+        }else{
+            res.redirect('/');
+        }
     });
 
     //Get all owners
     app.get('/owners', function (req, res) {
-        models.Owners.findAll({}).then(data => {
-            let Owners = [];
+        if(req.isAuthenticated()) {
+            models.Owners.findAll({}).then(data => {
+                let Owners = [];
 
-            for (owner in data) {
-                Owners.push(data[owner]);
-            }
+                for (owner in data) {
+                    Owners.push(data[owner]);
+                }
 
-            res.render('owners', {
-                owners: Owners,
-                isUser: req.isAuthenticated()
+                res.render('owners', {
+                    owners: Owners,
+                    isUser: req.isAuthenticated()
+                });
             });
-        });
+        }else{
+            res.redirect('/');
+        }
     });
 
     app.get('/pets/:id', function (req, res) {
-
-        models.Pets.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [models.Owners]
-        }).then(data => {
-            models.Owners.findOne({
+        if(req.isAuthenticated()) {
+            models.Pets.findOne({
                 where: {
-                    id: data.OwnerId
-                }
-            }).then(ownerData => {
-                models.Friendships.findAll({
-                    attributes: ['friendPetId'],
+                    id: req.params.id
+                },
+                include: [models.Owners]
+            }).then(data => {
+                models.Owners.findOne({
                     where: {
-                        myPetId: req.params.id
-                    },
-                    include: ['friendPet']
-                }).then(petsFriendsData => {
-                    console.log(ownerData.name);
-
-                    let Pets = [];
-
-                    for (pet in petsFriendsData) {
-                        console.log(petsFriendsData[pet].friendPet.name);
-                        console.log("============================");
-                        Pets.push(petsFriendsData[pet].friendPet);
+                        id: data.OwnerId
                     }
+                }).then(ownerData => {
+                    models.Friendships.findAll({
+                        attributes: ['friendPetId'],
+                        where: {
+                            myPetId: req.params.id
+                        },
+                        include: ['friendPet']
+                    }).then(petsFriendsData => {
+                        console.log(ownerData.name);
 
-                    res.render('pet', {
-                        id: req.params.id,
-                        name: data.name,
-                        picture: data.picture,
-                        age: data.age,
-                        type: data.type,
-                        breed: data.breed,
-                        bio: data.bio,
-                        location: ownerData.location,
-                        ownerName: ownerData.name,
-                        ownerAge: ownerData.age,
-                        ownerPicture: ownerData.picture,
-                        ownerId: ownerData.id,
-                        isUser: req.isAuthenticated(),
-                        userId: req.user.id,
-                        friendPets: Pets
+                        let Pets = [];
+
+                        for (pet in petsFriendsData) {
+                            console.log(petsFriendsData[pet].friendPet.name);
+                            console.log("============================");
+                            Pets.push(petsFriendsData[pet].friendPet);
+                        }
+
+                        res.render('pet', {
+                            id: req.params.id,
+                            name: data.name,
+                            picture: data.picture,
+                            age: data.age,
+                            type: data.type,
+                            breed: data.breed,
+                            bio: data.bio,
+                            location: ownerData.location,
+                            ownerName: ownerData.name,
+                            ownerAge: ownerData.age,
+                            ownerPicture: ownerData.picture,
+                            ownerId: ownerData.id,
+                            isUser: req.isAuthenticated(),
+                            userId: req.user.id,
+                            friendPets: Pets
+                        });
                     });
                 });
             });
-        });
+        }else{
+            res.redirect('/');
+        }
     });
 
     app.post('/pets/:id', function (req, res) {
@@ -140,36 +151,40 @@ module.exports = function (app) {
 
     //Get all pets
     app.get('/pets', function (req, res) {
-        models.Pets.findAll({
-            where: {
-                OwnerId: {$ne: req.user.id}
-            }
-        }).then(data => {
-            let Pets = [];
-            let Types = [];
-            let Breeds = [];
-
-            for (pet in data) {
-                Pets.push(data[pet]);
-
-                if (!Types.includes(data[pet].type)) {
-                    Types.push(data[pet].type);
+        if(req.isAuthenticated()) {
+            models.Pets.findAll({
+                where: {
+                    OwnerId: {$ne: req.user.id}
                 }
+            }).then(data => {
+                let Pets = [];
+                let Types = [];
+                let Breeds = [];
 
-                if (!Breeds.includes(data[pet].breed)) {
-                    if (data[pet].breed !== '') {
-                        Breeds.push(data[pet].breed);
+                for (pet in data) {
+                    Pets.push(data[pet]);
+
+                    if (!Types.includes(data[pet].type)) {
+                        Types.push(data[pet].type);
+                    }
+
+                    if (!Breeds.includes(data[pet].breed)) {
+                        if (data[pet].breed !== '') {
+                            Breeds.push(data[pet].breed);
+                        }
                     }
                 }
-            }
 
-            res.render('pets', {
-                pets: Pets,
-                types: Types,
-                breeds: Breeds,
-                isUser: req.isAuthenticated()
+                res.render('pets', {
+                    pets: Pets,
+                    types: Types,
+                    breeds: Breeds,
+                    isUser: req.isAuthenticated()
+                });
             });
-        });
+        }else{
+            res.redirect('/');
+        }
     });
 
     app.post('/pets', function (req, res) {
